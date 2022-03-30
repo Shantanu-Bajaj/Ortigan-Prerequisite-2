590e22c68e832b56a8e4a5f7a5adb85a800ec74a
Shantanu Bajaj <shantanubajaj05@gmail.com>
2022-03-30 22:32:15 +0530

    Added Documentation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''importing all required packages'''
 import requests
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
@@ -6,11 +7,11 @@ import clx.xms
 from clx.xms.api import MtBatchTextSmsCreate
 import json
 
-with open('config.json','r') as c:
-    params = json.load(c)["params"]
+with open('config.json','r') as c:      #opening json file in read mode
+    params = json.load(c)["params"]     #loading parameters from json file
 
-app = Flask(__name__)
-app.config.update(
+app = Flask(__name__)               #creating a Flask object
+app.config.update(                  #updating the Flask object
     MAIL_SERVER = 'smtp.gmail.com',
     MAIL_PORT = '465',
     MAIL_USE_SSL = True,
@@ -18,30 +19,31 @@ app.config.update(
     MAIL_PASSWORD = params['mail-pass']
 )
 
-mail = Mail(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
-db = SQLAlchemy(app)
+mail = Mail(app)                    #creating an object of Mail class from flask_mail
+app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']         #configuring Database Parameters
+db = SQLAlchemy(app)                #Creating SQLAlchemy object
 
-class Information(db.Model):
-    name = db.Column(db.String ,nullable = False)
-    email = db.Column(db.String, nullable = False)
-    phone = db.Column(db.Integer, primary_key = True)
+class Information(db.Model):                                #creating class for database Table
+    name = db.Column(db.String ,nullable = False)           #creating DM for name attribute
+    email = db.Column(db.String, nullable = False)          #creating DM for email attribute
+    phone = db.Column(db.Integer, primary_key = True)       #creating DM for phone attribute
 
-@app.route("/", methods = ['GET','POST'])
+@app.route("/", methods = ['GET','POST'])       #creating route for index page
 def home():
-    if(request.method == 'POST'):
-        '''Adding entry to database'''
-        NAME = request.form.get('name')
-        EMAIL = request.form.get('email')
-        PHONE = request.form.get('phone')
-        data = Information(name = NAME, email = EMAIL, phone = PHONE)
+    if(request.method == 'POST'):                   #checking if a request is POST or GET
+        NAME = request.form.get('name')             #retrieving name entered by user in form
+        EMAIL = request.form.get('email')           #retrieving email entered by user in form
+        PHONE = request.form.get('phone')           #retrieving phone entered by user in form
+        RECIPIENT = []                              #creating empty list of recipients
+        RECIPIENT.append(EMAIL)                     #adding user's email in list
+        data = Information(name = NAME, email = EMAIL, phone = PHONE)       #adding user's entered data in Database Table
         db.session.add(data)
-        db.session.commit()
-        mail.send_message('Email',
-                          sender = EMAIL,
-                          recipients = [params['mail']],
+        db.session.commit()                         #commiting session
+        mail.send_message('Email',                                          #code for sending the mail with required parameters
+                          sender = "shantanu.bajaj.30@gmail.com",
+                          recipients = RECIPIENT,
                           body = 'Email Id has been saved')
-        client = clx.xms.Client(service_plan_id='94f26b42db054f53a14de9714ced2754',
+        client = clx.xms.Client(service_plan_id='94f26b42db054f53a14de9714ced2754',     #code for sending an sms
                                 token='186484640ae441d3943be18ea44c081b')
         create = clx.xms.api.MtBatchTextSmsCreate()
         create.sender = str(PHONE)
@@ -53,6 +55,6 @@ def home():
         except (requests.exceptions.RequestException,
                 clx.xms.exceptions.ApiException) as ex:
             print('Failed to communicate with XMS: %s' % str(ex))
-    return render_template('index.html')
+    return render_template('index.html')                    #rendering index page
 
-app.run(debug=True)
+app.run(debug=True)                 #enabling debug mode so that changes are reflected in terminal
